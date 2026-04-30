08cad75328fd27906d66594875c2768e33c12830
Patil <b23cs1050@iitj.ac.in>
2026-04-30 22:12:18 +0530

    ls command implemented

--- san_shell.cpp
+++ san_shell.cpp
@@ -1,11 +1,50 @@
 #include<iostream> 
-#include<unistd>
+#include<unistd.h>
 #include<sys/wait.h>
 #include<sys/types.h>
 #include<cstdlib>
-int main(){
-  while(){
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<fcntl.h>
+#include<signal.h>
+#include<sstream>
+#include<fstream>
+#include<cstring> 
 
+int main(){
+  while(1){
+        std::cout<<"$ ";
+        std::string input;
+        getline(std::cin,input);
+	std::istringstream instr(input);
+        std::string word;
+	std::vector<std::string> tokens;
+	while(instr >> word) tokens.push_back(word);
+        if(tokens.empty()){
+		std::cout<<std::endl;
+		continue;          
+        }
+	if(*tokens.begin()=="exit") break;
+	std::vector<char*> args;
+	for(auto &s:tokens){
+		args.push_back(const_cast<char*>(s.c_str()));
+
+	}
+	args.push_back(nullptr);
+        pid_t pid = fork();
+	if(pid==0){
+		execvp(args[0],args.data());
+		perror("exec failed");
+		exit(1);
+	}
+	else if(pid>0){
+		wait(NULL);	
+	}
+        else{
+		perror("fork failed");
+	}
   }
+  return 0;
 
 }
